264054f7fd993a88bb69229c7160b1f3e9195984
Trish Polay <trishp@imaginet.com>
2014-04-22 16:23:34 -0500

    Added time frame drop down

--- HealthConnect.Mobile/scripts/history.js
+++ HealthConnect.Mobile/scripts/history.js
@@ -1,30 +1,39 @@
 (function (global) {
-    var HistoryViewModel,
         app = global.app = global.app || {};
-
-    HistoryViewModel = kendo.data.ObservableObject.extend({
+    
+    app.historyService = {   
+      viewModel: kendo.observable({
+        selectedTimeFrame: {name: "1 week", value: "7"},
+        timeFrames: [
+        {name: "1 week", value: "7"}, 
+        {name: "30 days", value: "30"},
+        {name: "60 days", value: "60"},
+        {name: "90 days", value: "90"},
+        {name: "All", value: "0"}
+        ],
+        onTimeFrameChange: function(e){
+            debugger;
+            app.historyService.history.read({timeFrame: this.get("selectedTimeFrame").value});
+        },        
         listViewClick: function (e) {
             var hidden = $(e.item).find("ul").is(':hidden');
              $('.history-details').hide();
              if(hidden === true) {
                  $(e.item).find("ul").show();
             }
-           },
-        
+           },        
         renderDetailsTemplate: function(data) {
     		return kendo.Template.compile($('#history-details-template').html())(data);
 			}
-        
-        });
-    
-    
-
-    app.historyService = {
-        viewModel: new HistoryViewModel(),
-        history: new kendo.data.DataSource.create({
-            transport: {
-                read: "http://localhost:5286/Api/Mobile/History"
-            }
         }),
+      history: new kendo.data.DataSource.create({
+            transport: {
+                read: {
+                    url:"http://localhost:5286/Api/Mobile/History",
+                	data: {timeFrame: "7"}
+                    }
+            }
+        })
     }
+    
 })(window);
