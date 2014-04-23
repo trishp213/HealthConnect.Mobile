8d5da1bf19ffd7cb508fa155ecf11ac824edc4a6
Trish Polay <trishp@imaginet.com>
2014-04-23 13:51:26 -0500

    Moved files into views folder

--- HealthConnect.Mobile/scripts/app.js
+++ HealthConnect.Mobile/scripts/app.js
@@ -1,4 +1,4 @@
 (function (global) {
     var app = global.app = global.app || {};
-    app.application = new kendo.mobile.Application(document.body, { layout: "drawer-layout", initial: "login.html"});
+    app.application = new kendo.mobile.Application(document.body, { layout: "drawer-layout", initial: "views/login.html"});
 })(window);
