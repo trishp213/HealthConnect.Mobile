f62f4c434ec079e6b723cf97a2e5013c1d850be9
Trish Polay <trishp@imaginet.com>
2014-04-24 10:30:53 -0500

    Refactored out timeFrames script

--- HealthConnect.Mobile/scripts/app/history.js
+++ HealthConnect.Mobile/scripts/app/history.js
@@ -4,18 +4,6 @@
     app.historyService = {   
       viewModel: kendo.observable({
         selectedTimeFrame: {name: "1 week", value: "7"},
-        timeFrames: [
-        {name: "1 week", value: "7"}, 
-        {name: "30 days", value: "30"},
-        {name: "60 days", value: "60"},
-        {name: "90 days", value: "90"},
-        {name: "All", value: "0"}
-        ],
-        onTimeFrameChange: function(e){
-            app.historyService.viewModel.set("selectedTimeFrame", e.dataItem)
-            app.application.navigate("views/history.html");
-            app.historyDataSource.read({timeFrame: e.dataItem.value});
-        },        
         listViewClick: function (e) {
             var hidden = $(e.item).find("ul").is(':hidden');
              $('.history-details').hide();
@@ -23,9 +11,15 @@
                  $(e.item).find("ul").show();
             }
            },        
-        renderDetailsTemplate: function(data) {
+         renderDetailsTemplate: function(data) {
     		return kendo.Template.compile($('#history-details-template').html())(data);
-			}
+			},
+          refreshData: function(e) {
+              if(e.view.params.timeFrameValue) {
+            	app.historyService.viewModel.set("selectedTimeFrame", {name: e.view.params.timeFrameName, value: e.view.params.timeFrameValue});
+                }
+              app.historyDataSource.read({timeFrame: app.historyService.viewModel.get("selectedTimeFrame").value});
+          }
         })      
     }
 
