568fd28e0e98b8b1c9e44999469c4616b9cd81fe
Trish Polay <trishp@imaginet.com>
2014-05-02 16:50:02 -0500

    Cleaned up appointments view

--- HealthConnect.Mobile/scripts/app/appointments.js
+++ HealthConnect.Mobile/scripts/app/appointments.js
@@ -1,19 +1,17 @@
 (function (global) {
-    app = global.app = global.app || {};
-    
-    
+    app = global.app = global.app || {};   
     
     app.appointmentsDataSource = new kendo.data.DataSource.create({
       transport: {
 			read: {
                   url:"http://localhost:5286/Api/Mobile/Appointments",
               }
-        }
+        	}
         });
     
     var dayDisplayFormat = "dddd, MMM d";
     var monthDisplayFormat = "MMMM yyyy";    
-    var dateFormat = "MMM dd, yyyy";
+    var dateFormat = "MMM dd, yyyy"; // kendo date formats are invalid for javascript, so need to pick a format and toString the dates before working with them
 
     app.appointmentsService = {   
         viewModel: kendo.observable({
@@ -25,13 +23,19 @@
         noData: true,
         updateDaysOfWeek: function () {
             var daysOfWeek = new Array();
+            
+            // get currently selected date - toString required because kendo dates are in invalid formats for javascript
             var sunday = Date.parse(app.appointmentsService.viewModel.selectedDate.toString(dateFormat));
+            
+            // move to sunday of the current week
             if (sunday.is().sunday() === false) {
                 sunday = sunday.last().sunday();
             }
             
+            // save the sunday as the start date of the week
             app.appointmentsService.viewModel.set("startDate", sunday.toString(dateFormat));
 
+            // add each day of the week to the list
             for (var i = 0; i < 7; i++) {
                 daysOfWeek.push({ 
                     day: sunday.toString("ddd"), 
@@ -40,42 +44,54 @@
                 	dateValue: sunday.toString(dayDisplayFormat)});
                 sunday.add(1).days();
             }
+            // save the following sunday as the last day of the week
             app.appointmentsService.viewModel.set("endDate", sunday.toString(dateFormat));
             
+            // push the dates to the datasource that the date selector is bound to
             app.appointmentsService.viewModel.daysOfWeek.data(daysOfWeek);
+            
+            // update the month picker 
             app.appointmentsService.viewModel.set("month", app.appointmentsService.viewModel.selectedDate.toString(monthDisplayFormat));
+            
+            // get the appointments for that week from the service
             app.appointmentsDataSource.read(app.appointmentsService.viewModel.getDateRange());
         },
 	    getDateRange: function(){
+            // get the dates for the start and end of the week to pass to the service
           return { startDate: app.appointmentsService.viewModel.startDate, endDate: app.appointmentsService.viewModel.endDate};
         },
         renderDetailsTemplate: function(data) {
             return kendo.Template.compile($('#appointments-details-template').html())(data);
         },
-        refreshData: function(e) {
+        onShow: function(e) {
             app.appointmentsDataSource.read(app.appointmentsService.viewModel.getDateRange());
             $("#appointments-scroller").data("kendoMobileScroller").reset();
         },
         onClick: function(e) {
+            // remove the hilighting currently selected dates
             $("li").removeClass("selectedGroup");
-            $(".listHeader").removeClass("selectedGroup"); // unhilite all
+            $(".listHeader").removeClass("selectedGroup"); 
+            
+            // hilight the current day of week in the date slider
             e.item.addClass("selectedGroup");
             var selectedDate = e.item.find(".day-of-week-value").val();
             var selectedDateListItem = $(".listHeader:contains(" + selectedDate + ")");
             selectedDateListItem.addClass("selectedGroup");
             app.appointmentsService.viewModel.set("month", e.item.find(".day-of-week-month").val());
             
-            // scroll to selected date in list view
+            // scroll tback to the top
             var scroller = $("#appointments-scroller").data("kendoMobileScroller");
             scroller.reset();
             
+            // get the position of the selected date in the listview
             var pos = $(selectedDateListItem).offset();
             if(pos){
+                // scroll to the selected date in the listview
             	scroller.scrollTo(-pos.left, -pos.top + $("#appointments-header").height() + selectedDateListItem.height());
                 }
         },
         onSwipeMonth: function(e) {
-            
+            // move ahead or behind a month
             var monthsToAdd = 1;
             if (e.direction === "right") {
                 monthsToAdd = -1;                                                
@@ -84,16 +100,20 @@
             currentDate = currentDate.add(monthsToAdd).months();
             app.appointmentsService.viewModel.set("month", currentDate.toString(monthDisplayFormat));
             
+            // if moving ahead, move to the first day of the month
+            // if moving back a month, move to the last day of the month
             if(monthsToAdd === 1) {
                 currentDate = currentDate.moveToFirstDayOfMonth();
             }
             else {
                 currentDate = currentDate.moveToLastDayOfMonth();
             }
-            app.appointmentsService.viewModel.set("selectedDate", kendo.parseDate(currentDate.toString(dateFormat), dateFormat));
             
+            // update the selected date
+            app.appointmentsService.viewModel.set("selectedDate", kendo.parseDate(currentDate.toString(dateFormat), dateFormat));
         },
         onWeekSwipe: function(e) {
+            // move ahead of back a week
             var currentlySelectedDate = Date.parse(app.appointmentsService.viewModel.selectedDate.toString(dateFormat));
 
             if (e.direction === "right") { // going to previous week, set selected date to last saturday
@@ -103,6 +123,7 @@
             }
         },
         onDataBound: function(e) {
+            // show no availability mesasge if no records
             if(this.dataSource.data().length === 0) {
                 app.appointmentsService.viewModel.set("noData", true);
             }
@@ -110,6 +131,7 @@
                 app.appointmentsService.viewModel.set("noData", false);
             }
             
+            // hilight the selected date
             var selectedDate = app.appointmentsService.viewModel.selectedDate.toString(dayDisplayFormat);
             $("input[value='" + selectedDate + "']").closest("li").addClass("selectedGroup");
             app.appointmentsService.viewModel.setScrollerHeight();
@@ -117,16 +139,18 @@
             var selectedDateListItem = $(".listHeader:contains(" + selectedDate + ")");
             selectedDateListItem.addClass("selectedGroup");
             
-            // scroll to selected date in list view
+            // scroll to the top
             var scroller = $("#appointments-scroller").data("kendoMobileScroller");
             scroller.reset();
             
             var pos = $(selectedDateListItem).offset();
             if(pos){
+                // scroll to the selected date in the list
             	scroller.scrollTo(-pos.left, -pos.top + $("#appointments-header").height() + selectedDateListItem.height());
                 }
         },
         setScrollerHeight: function(e) {
+            // set the scroller to the correct height depending on the position of the device
             $("#appointments-scroller").css("height", $(window).height() - $("#appointments-header").height());
         }
       })
